79f70f2cf81884ee0226732776e198b7ceac000b
Ege Furkan Bilgin <furkanegebilgin@gmail.com>
2025-07-10 17:04:41 +0300

    Some hex grid util classes underway

--- Grids/SpatialHelpers/AxialCoordinateHash.cs
+++ Grids/SpatialHelpers/AxialCoordinateHash.cs
@@ -12,11 +12,11 @@ namespace Frolics.Grids.SpatialHelpers {
 		private readonly Vector2Int[] hexSelectionOffsets = {
 			new(0, 0),
 			new(1, 0),
+			new(1, 1),
 			new(0, 1),
-			new(1, -1),
-			new(0, -1),
 			new(-1, 0),
-			new(-1, 1),
+			new(-1, -1),
+			new(0, -1),
 		};
 
 		private readonly Dictionary<Vector2Int, T> axialMap;
@@ -74,9 +74,9 @@ namespace Frolics.Grids.SpatialHelpers {
 			float y = worldPosition.y / size;
 
 			// Cartesian to hex
-			float q = Mathf.Sqrt(3f) / 3f * x - 1f / 3f * y;
-			float r = 2f / 3f * y;
-			return AxialRound(q, r);
+			float fractionalQ = Mathf.Sqrt(3f) / 3f * x - 1f / 3f * y - 2;
+			float fractionalR = 2f / 3f * y - 2;
+			return AxialRound(fractionalQ, fractionalR);
 		}
 
 		private Vector2Int AxialRound(float fractionalQ, float fractionalR) {
@@ -103,13 +103,6 @@ namespace Frolics.Grids.SpatialHelpers {
 
 			Assert.IsTrue(roundedQ + roundedR + roundedS == 0);
 			return new Vector2Int(roundedQ, roundedR);
-		}
-
-		public Vector2Int AxialToOddR(Vector2Int hex) {
-			int parity = hex.y & 1; // 0 if even, 1 if odd
-			int col = hex.x + (hex.y - parity) / 2;
-			int row = hex.y;
-			return new Vector2Int(col, row);
 		}
 
 		public bool TryGetCell(Vector3 worldPosition, out T fieldCell) {
@@ -143,44 +136,220 @@ namespace Frolics.Grids.SpatialHelpers {
 	}
 
 
-	// // IDEA Rename to Axial
-	// public struct Hex {
-	// 	public static readonly Hex[] SelectionOffsets = {
-	// 		new(0, 0),
-	// 		new(1, 0),
-	// 		new(1, 1),
-	// 		new(0, 1),
-	// 		new(-1, 0),
-	// 		new(-1, -1),
-	// 		new(0, -1)
-	// 	};
-	//
-	// 	public int Q { get; private set; }
-	// 	public int R { get; private set; }
-	//
-	// 	public Hex(int q, int r) {
-	// 		Q = q;
-	// 		R = r;
-	// 	}
-	//
-	// 	public Hex(Vector2Int axial) {
-	// 		Q = axial.x;
-	// 		R = axial.y;
-	// 	}
-	//
-	//
-	// 	public static int GetAxialDistance(Hex a, Hex b) {
-	// 		int deltaQ = a.Q - b.Q;
-	// 		int deltaR = a.R - b.R;
-	// 		return (Mathf.Abs(deltaQ) + Mathf.Abs(deltaR) + Mathf.Abs(deltaQ + deltaR)) / 2;
-	// 	}
-	//
-	// 	public static Hex operator +(Hex a, Hex b) {
-	// 		return new Hex(a.Q + b.Q, a.R + b.R);
-	// 	}
-	//
-	// 	public static Hex operator -(Hex a, Hex b) {
-	// 		return new Hex(a.Q - b.Q, a.R - b.R);
-	// 	}
-	// }
+	public struct AxialCoord {
+		public int q;
+		public int r;
+
+		public static AxialCoord[] directionVectors = {
+			new AxialCoord(1, 0), // East
+			new AxialCoord(0, 1), // North East
+			new AxialCoord(-1, 1), // North West
+			new AxialCoord(-1, 0), // West
+			new AxialCoord(0, -1), // South West
+			new AxialCoord(1, -1) // South East
+		};
+
+		public AxialCoord(int q, int r) {
+			this.q = q;
+			this.r = r;
+		}
+
+		public AxialCoord GetDirection(int directionIndex) {
+			return directionVectors[directionIndex];
+		}
+
+		public AxialCoord GetNeighbor(AxialCoord center, int neighborIndex) {
+			return center + GetDirection(neighborIndex);
+		}
+
+
+		// Operator overloads
+		public static AxialCoord operator +(AxialCoord lhs, AxialCoord rhs) => new(lhs.q + rhs.q, lhs.r + rhs.r);
+		public static AxialCoord operator -(AxialCoord lhs, AxialCoord rhs) => new(lhs.q - rhs.q, lhs.r - rhs.r);
+
+
+		// Optional: equality support
+		public override bool Equals(object obj) => obj is AxialCoord other && q == other.q && r == other.r;
+		public override int GetHashCode() => new Vector2Int(q, r).GetHashCode();
+		public static bool operator ==(AxialCoord lhs, AxialCoord rhs) => lhs.q == rhs.q && lhs.r == rhs.r;
+		public static bool operator !=(AxialCoord lhs, AxialCoord rhs) => !(lhs == rhs);
+
+		// TODO Migrate this to an extension class
+		public static CubeCoord ToCubeCoord(AxialCoord axialCoord) {
+			int cubeQ = axialCoord.q;
+			int cubeR = axialCoord.r;
+			int cubeS = -axialCoord.q - axialCoord.r;
+			return new CubeCoord(cubeQ, cubeR, cubeS);
+		}
+	}
+
+	public struct DoubledCoord {
+		public int column;
+		public int row;
+
+
+		public DoubledCoord(int column, int row) {
+			this.column = column;
+			this.row = row;
+		}
+
+
+		public static AxialCoord DoubleHeightToAxial(DoubledCoord doubledCoord) {
+			int q = doubledCoord.column;
+			int r = (doubledCoord.row - doubledCoord.column) / 2;
+			return new AxialCoord(q, r);
+		}
+
+		public static DoubledCoord AxialToDoubleHeight(AxialCoord axialCoord) {
+			int column = axialCoord.q;
+			int row = 2 * axialCoord.r + axialCoord.q;
+			return new DoubledCoord(column, row);
+		}
+
+		public static AxialCoord DoubleWidthToAxial(DoubledCoord doubledCoord) {
+			int q = (doubledCoord.column - doubledCoord.row) / 2;
+			int r = doubledCoord.row;
+			return new AxialCoord(q, r);
+		}
+
+		public static DoubledCoord AxialToDoubleWidth(AxialCoord axialCoord) {
+			int column = 2 * axialCoord.q + axialCoord.r;
+			int row = axialCoord.r;
+			return new DoubledCoord(column, row);
+		}
+	}
+
+	public struct CubeCoord {
+		public int q;
+		public int r;
+		public int s;
+
+		public static CubeCoord[] directionVectors = {
+			new CubeCoord(1, 0, -1), // East
+			new CubeCoord(0, 1, -1), // North East
+			new CubeCoord(-1, 1, 0), // North West
+			new CubeCoord(-1, 0, 1), // West
+			new CubeCoord(0, -1, 1), // South West
+			new CubeCoord(1, -1, 0) // South East
+		};
+
+		public CubeCoord(int q, int r, int s) {
+			this.q = q;
+			this.r = r;
+			this.s = s;
+		}
+
+		public CubeCoord GetDirection(int directionIndex) {
+			return directionVectors[directionIndex];
+		}
+
+		public CubeCoord GetNeighbor(CubeCoord center, int neighborIndex) {
+			return center + GetDirection(neighborIndex);
+		}
+
+		// Operator overloads
+		public static CubeCoord operator +(CubeCoord lhs, CubeCoord rhs) {
+			return new CubeCoord(lhs.q + rhs.q, lhs.r + rhs.r, lhs.s + rhs.s);
+		}
+
+		public static CubeCoord operator -(CubeCoord lhs, CubeCoord rhs) {
+			return new CubeCoord(lhs.q - rhs.q, lhs.r - rhs.r, lhs.s - rhs.s);
+		}
+
+
+		// Optional: equality support
+		public override bool Equals(object obj) {
+			return obj is CubeCoord other && q == other.q && r == other.r && s == other.s;
+		}
+
+		public override int GetHashCode() => new Vector3Int(q, r, s).GetHashCode();
+
+		public static bool operator ==(CubeCoord lhs, CubeCoord rhs) {
+			return lhs.q == rhs.q && lhs.r == rhs.r && lhs.s == rhs.s;
+		}
+
+		public static bool operator !=(CubeCoord lhs, CubeCoord rhs) => !(lhs == rhs);
+
+		// TODO Migrate this to an extension class
+		public static AxialCoord ToAxialCoord(CubeCoord cubeCoord) {
+			int axialQ = cubeCoord.q;
+			int axialR = cubeCoord.r;
+			return new AxialCoord(axialQ, axialR);
+		}
+	}
+
+	// Only odd-r
+	public struct OffsetCoord {
+		public int column;
+		public int row;
+
+		public OffsetCoord(int column, int row) {
+			this.column = column;
+			this.row = row;
+		}
+
+		// TODO Migrate this to an extension class
+		// *-r are pointy top; *-q are flat top.
+
+		// Pointy-top, shoves odd rows by +1/2 column
+		public static OffsetCoord AxialToOddR(AxialCoord axialCoord) {
+			int parity = axialCoord.r & 1; // 0 if even, 1 if odd
+			int column = axialCoord.q + (axialCoord.r - parity) / 2;
+			int row = axialCoord.r;
+			return new OffsetCoord(column, row);
+		}
+
+		public static AxialCoord OddRToAxial(OffsetCoord offsetCoord) {
+			int parity = offsetCoord.row & 1;
+			int q = offsetCoord.column - (offsetCoord.row - parity) / 2;
+			int r = offsetCoord.row;
+			return new AxialCoord(q, r);
+		}
+
+		// Pointy-top, shoves even rows by +1/2 column
+		public static OffsetCoord AxialToEvenR(AxialCoord axialCoord) {
+			int parity = axialCoord.r & 1; // 0 if even, 1 if odd
+			int column = axialCoord.q + (axialCoord.r + parity) / 2;
+			int row = axialCoord.r;
+			return new OffsetCoord(column, row);
+		}
+
+		public static AxialCoord EvenRToAxial(OffsetCoord offsetCoord) {
+			int parity = offsetCoord.row & 1;
+			int q = offsetCoord.column - (offsetCoord.row + parity) / 2;
+			int r = offsetCoord.row;
+			return new AxialCoord(q, r);
+		}
+
+
+		// Flat-top, shoves odd columns by +1/2 column
+		public static OffsetCoord AxialToOddQ(AxialCoord axialCoord) {
+			int parity = axialCoord.q & 1; // 0 if even, 1 if odd
+			int column = axialCoord.q;
+			int row = axialCoord.r + (axialCoord.q - parity) / 2;
+			return new OffsetCoord(column, row);
+		}
+
+		public static AxialCoord OddQToAxial(OffsetCoord offsetCoord) {
+			int parity = offsetCoord.column & 1;
+			int q = offsetCoord.column;
+			int r = offsetCoord.row - (offsetCoord.column - parity) / 2;
+			return new AxialCoord(q, r);
+		}
+
+		// Flat-top, shoves even columns by +1/2 column
+		public static OffsetCoord AxialToEvenQ(AxialCoord axialCoord) {
+			int parity = axialCoord.q & 1; // 0 if even, 1 if odd
+			int column = axialCoord.q;
+			int row = axialCoord.r + (axialCoord.q + parity) / 2;
+			return new OffsetCoord(column, row);
+		}
+
+		public static AxialCoord EvenQToAxial(OffsetCoord offsetCoord) {
+			int parity = offsetCoord.column & 1;
+			int q = offsetCoord.column;
+			int r = offsetCoord.row - (offsetCoord.column + parity) / 2;
+			return new AxialCoord(q, r);
+		}
+	}
 }
