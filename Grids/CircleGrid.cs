646f34cbf7b9fccf9ce187b04ce30fb42db7df5a
Ege Furkan Bilgin <furkanegebilgin@gmail.com>
2025-08-19 20:54:33 +0300

    CircleGrid with axial operations

--- Grids/CircleGrid.cs
+++ Grids/CircleGrid.cs
@@ -1,10 +1,15 @@
+using System.Collections.Generic;
 using Frolics.Grids.NeighborHelpers;
+using Frolics.Grids.SpatialHelpers;
 using Frolics.Utilities;
 using UnityEngine;
 
 namespace Frolics.Grids {
 	public class CircleGrid<T> : Grid<T> where T : CircleCell {
-		private readonly CircleGridNeighborHelper<T> neighborHelper;
+		// private readonly CircleGridNeighborHelper<T> neighborHelper;
+
+		private readonly Dictionary<AxialCoord, T> cellsByAxialCoord;
+		private readonly Dictionary<T, AxialCoord> axialCoordsByCell;
 
 		// NOTE HexGrid<CircleCell> HexGrid<HexCell> initialized via AxialCoordinates (Doubled or Offset)
 		protected CircleGrid(CellFactory<T> cellFactory, Vector2Int gridSize, float cellDiameter) {
@@ -16,40 +21,7 @@ namespace Frolics.Grids {
 			this.centerPoint = CalculateGridCenterPoint(cellPositions);
 			this.cells = GenerateCells(cellFactory, cellPositions);
 
-			this.neighborHelper = new CircleGridNeighborHelper<T>(this);
-		}
-
-		private T[] GenerateCells(CellFactory<T> cellFactory, Vector2[] cellPositions) {
-			T[] cells = new T[cellPositions.Length];
-
-			for (int i = 0; i < cellPositions.Length; i++)
-				cells[i] = cellFactory.Create(cellPositions[i], cellDiameter);
-
-			return cells;
-		}
-
-		private Vector2[] GenerateCellPositions(Vector2Int gridSizeInCells) {
-			int evenRowCount = Mathf.CeilToInt(gridSizeInCells.y / 2f);
-			Vector2[] cellPositions = new Vector2[gridSizeInCells.x * gridSizeInCells.y + evenRowCount];
-			Vector2 cellSpacing = new(cellDiameter, cellDiameter * Mathf.Cos(30 * Mathf.Deg2Rad));
-
-			Vector2Int index = Vector2Int.zero;
-
-			for (index.y = 0; index.y < gridSizeInCells.y; index.y++) {
-				Vector2 cellOffset = new(index.y % 2 == 0 ? 0 : (cellDiameter / 2), 0f);
-				int rowSizeInCells = index.y % 2 == 0 ? (gridSizeInCells.x + 1) : gridSizeInCells.x;
-
-				for (index.x = 0; index.x < rowSizeInCells; index.x++) {
-					float cellPosX = cellOffset.x + index.x * cellSpacing.x;
-					float cellPosY = cellOffset.y + index.y * cellSpacing.y;
-
-					int evenRowsPassed = Mathf.CeilToInt(index.y / 2f);
-					int positionIndex = index.x + index.y * gridSizeInCells.x + evenRowsPassed;
-					cellPositions[positionIndex] = new Vector2(cellPosX, cellPosY);
-				}
-			}
-
-			return cellPositions;
+			// this.neighborHelper = new CircleGridNeighborHelper<T>(this);
 		}
 
 		private Vector2 GetFittingGridSize(Vector2Int gridSizeInCells) {
@@ -57,6 +29,67 @@ namespace Frolics.Grids {
 			float sizeY = (gridSizeInCells.y - 1) * cellDiameter * Mathf.Cos(30 * Mathf.Deg2Rad);
 			return new Vector2(sizeX, sizeY);
 		}
+
+
+		private Vector2[] GenerateCellPositions(Vector2Int gridSize) {
+			int evenRowCount = Mathf.CeilToInt(gridSize.y / 2f);
+			Vector2[] cellPositions = new Vector2[gridSize.x * gridSize.y + evenRowCount];
+
+			for (int y = 0; y < gridSize.y; y++) {
+				int rowSizeInCells = y % 2 == 0 ? (gridSize.x + 1) : gridSize.x;
+
+				for (int x = 0; x < rowSizeInCells; x++) {
+					Debug.Log($"x,y:{x},{y}");
+					OffsetOddRCoord oddRCoord = new(x, -y);
+					Vector2 worldPosition = AxialCoord.AxialToWorld(oddRCoord.ToAxial(), cellDiameter);
+
+					int evenRowsPassed = Mathf.CeilToInt(y / 2f);
+					int positionIndex = x + y * gridSize.x + evenRowsPassed;
+					cellPositions[positionIndex] = worldPosition;
+				}
+			}
+
+			return cellPositions;
+		}
+
+		private T[] GenerateCells(CellFactory<T> cellFactory, Vector2Int gridSize) {
+			int evenRowCount = Mathf.CeilToInt(gridSize.y / 2f);
+			T[] cells = new T[gridSize.x * gridSize.y + evenRowCount];
+
+			for (int y = 0; y < gridSize.y; y++) {
+				int rowSizeInCells = y % 2 == 0 ? (gridSize.x + 1) : gridSize.x;
+
+				for (int x = 0; x < rowSizeInCells; x++) {
+					OffsetOddRCoord oddRCoord = new(x, -y);
+					AxialCoord axialCoord = oddRCoord.ToAxial();
+					Vector2 worldPosition = AxialCoord.AxialToWorld(axialCoord, cellDiameter);
+
+					int evenRowsPassed = Mathf.CeilToInt(y / 2f);
+					int positionIndex = x + y * gridSize.x + evenRowsPassed;
+					T cell = cellFactory.Create(worldPosition, cellDiameter);
+					cells[positionIndex] = cell;
+
+					cellsByAxialCoord.Add(axialCoord, cell);
+					axialCoordsByCell.Add(cell, axialCoord);
+				}
+			}
+
+			return cells;
+		}
+
+		public bool TryGetCell(Vector3 worldPosition, out T cell) {
+			AxialCoord centerAxial = AxialCoord.WorldToAxial(worldPosition, cellDiameter);
+			return cellsByAxialCoord.TryGetValue(centerAxial, out cell);
+		}
+
+		public bool TryGetCell(AxialCoord axialCoord, out T cell) {
+			return cellsByAxialCoord.TryGetValue(axialCoord, out cell);
+		}
+
+		public AxialCoord GetAxialCoordinates(T cell) {
+			return axialCoordsByCell.GetValueOrDefault(cell);
+		}
+
 
 		private Vector3 CalculateGridCenterPoint(Vector2[] cellPositions) {
 			Vector2 positionSum = Vector3.zero;
@@ -66,10 +99,10 @@ namespace Frolics.Grids {
 
 			return (positionSum / cellPositions.Length).WithZ(0f);
 		}
-
-		public T[] GetNeighbors(T cell) {
-			return neighborHelper.GetCellNeighbors(cell);
-		}
+		//
+		// public T[] GetNeighbors(T cell) {
+		// 	return neighborHelper.GetCellNeighbors(cell);
+		// }
 
 		// Redundant
 		private T GetCell(Vector2Int index) {
