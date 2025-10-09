95e30a8cc211bee94ce334f05d2fd53484ecac09
Ege Furkan Bilgin <furkanegebilgin@gmail.com>
2025-10-09 23:54:58 +0300

    SquareCoord: CustomPropertyDrawer copies Vector2Int for consistency

--- Grids/Editor/SquareCoordPropertyDrawer.cs
+++ Grids/Editor/SquareCoordPropertyDrawer.cs
@@ -8,26 +8,15 @@ namespace Frolics.Grids.Editor {
 		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
 			EditorGUI.BeginProperty(position, label, property);
 
-			// Draw label
-			position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
-
-			// Get the x and y properties
 			SerializedProperty xProp = property.FindPropertyRelative("x");
 			SerializedProperty yProp = property.FindPropertyRelative("y");
 
-			// Split the rect for two fields
-			float fieldWidth = position.width / 2 - 2;
+			Vector2Int vec = new(xProp.intValue, yProp.intValue);
+			vec = EditorGUI.Vector2IntField(position, label, vec);
 
-			// Draw X field
-			EditorGUIUtility.labelWidth = 15;
-			Rect xRect = new Rect(position.x, position.y, fieldWidth, position.height);
-			EditorGUI.PropertyField(xRect, xProp, new GUIContent("X"));
+			xProp.intValue = vec.x;
+			yProp.intValue = vec.y;
 
-			// Draw Y field
-			Rect yRect = new Rect(position.x + fieldWidth + 4, position.y, fieldWidth, position.height);
-			EditorGUI.PropertyField(yRect, yProp, new GUIContent("Y"));
-
-			EditorGUIUtility.labelWidth = 0;
 			EditorGUI.EndProperty();
 		}
 	}
