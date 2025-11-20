487ced7a552aab3f6cc31be5dabfaf47b053f4fe
Ege Furkan Bilgin <furkanegebilgin@gmail.com>
2025-11-20 16:18:25 +0300

    Improvement: HashList, added some set operations

--- Collections/Generic/HashList.cs
+++ Collections/Generic/HashList.cs
@@ -1,12 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
 namespace Frolics.Collections.Generic {
+	// TODO IHashList interface
 	public class HashList<T> : IEnumerable<T> {
-		private readonly HashSet<T> hashSet = new();
-		private readonly List<T> list = new();
+		private readonly HashSet<T> hashSet;
+		private readonly List<T> list;
 
 		public int Count => list.Count;
+		public T this[int index] => list[index];
+
+		public HashList(int capacity = 4) {
+			hashSet = new HashSet<T>(capacity);
+			list =  new List<T>(capacity);
+		}
 
 		public bool TryAdd(T item) {
 			if (!hashSet.Add(item))
@@ -20,6 +28,7 @@ namespace Frolics.Collections.Generic {
 			if (!hashSet.Remove(item))
 				return false;
 
+			// Remove from list (O(n))
 			list.Remove(item);
 			return true;
 		}
@@ -29,17 +38,45 @@ namespace Frolics.Collections.Generic {
 			list.Clear();
 		}
 
+		// --- Set Operations ---
 		public void IntersectWith(IEnumerable<T> other) {
+			if (other is null)
+				throw new ArgumentNullException(nameof(other));
+
 			hashSet.IntersectWith(other);
+			list.RemoveAll(item => !hashSet.Contains(item));
 		}
 
+		public void UnionWith(IEnumerable<T> other) {
+			if (other is null)
+				throw new ArgumentNullException(nameof(other));
+
+			foreach (T item in other)
+				TryAdd(item);
+		}
+
+		public void ExceptWith(IEnumerable<T> other) {
+			if (other is null)
+				throw new ArgumentNullException(nameof(other));
+
+			foreach (T item in other)
+				TryRemove(item);
+		}
+
+		public void SymmetricExceptWith(IEnumerable<T> other) {
+			if (other is null)
+				throw new ArgumentNullException(nameof(other));
+
+			foreach (T item in other) {
+				if (!TryRemove(item))
+					TryAdd(item);
+			}
+		}
+
+		public bool Contains(T item) => hashSet.Contains(item);
+		public int IndexOf(T item) => list.IndexOf(item);
 
 		public IEnumerator<T> GetEnumerator() => list.GetEnumerator();
 		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-		public int IndexOf(T item) => list.IndexOf(item);
-		public bool Contains(T item) => hashSet.Contains(item);
-
-		public T this[int index] => list[index];
 	}
 }
